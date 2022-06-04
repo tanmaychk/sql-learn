3816f72012c6cd71194b7dbe4434fb97d843328d
Tanmay Chakrabarty <70465237+tanmaychk@users.noreply.github.com>
2022-06-04 23:02:13 +0530

    added triggertest

---
+++ trigger_test.sql
@@ -0,0 +1,13 @@
+-- CREATE------------------------------------------------------------------------------------
+--     TRIGGER `event_name` BEFORE/AFTER INSERT/UPDATE/DELETE
+--     ON `database`.`table`
+--     FOR EACH ROW BEGIN
+-- 		-- trigger body
+-- 		-- this code is applied to every
+-- 		-- inserted/updated/deleted row
+--     END;
+
+CREATE TABLE trigger_test (
+     message VARCHAR(100)
+);
+--In cmd change delimiter--
